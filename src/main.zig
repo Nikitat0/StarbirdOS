3d59409307f12c42fc81da1b834f406be9ca8eb4
Nikitat0 <nikitatzero@yandex.ru>
2024-10-24 21:13:27 +0700

    Remove legacy debug clearing

--- src/main.zig
+++ src/main.zig
@@ -15,8 +15,6 @@ export fn _start() callconv(.Naked) noreturn {
 const LOGO: []const u8 = @embedFile("./logo.txt");
 
 fn kernel_main() noreturn {
-    const addr: *u16 = @ptrFromInt(0xb8000);
-    addr.* = 0;
     vga.disableCursor();
     var console = VgaConsole.obtain();
     console.writer().print("{s}", .{LOGO}) catch {};
